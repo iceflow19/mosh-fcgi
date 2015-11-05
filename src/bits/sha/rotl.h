3f3aa249ea8e4390396aa1d5a6cd1af79548f46f
Thomas Royko <iceflow19@gmail.com>
2015-11-05 08:57:30 -0500

    Removing autotools crap and adding cmake instead.  Fixed rotl

--- src/bits/sha/rotl.h
+++ src/bits/sha/rotl.h
@@ -13,42 +13,6 @@
 
 #elif !defined(__STRICT_ANSI__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__)) && !defined(INTEL_CC) && !defined(LTC_NO_ASM)
 
-static inline uint32_t ROL(uint32_t word, int i)
-{
-   asm ("roll %%cl,%0"
-      :"=r" (word)
-      :"0" (word),"c" (i));
-   return word;
-}
-
-static inline uint32_t ROLc(uint32_t word, const int i)
-{
-   asm ("roll %2,%0"
-      :"=r" (word)
-      :"0" (word),"I" (i));
-   return word;
-}
-
-#elif !defined(__STRICT_ANSI__) && defined(LTC_PPC32)
-
-static inline uint32_t ROL(uint32_t word, int i)
-{
-   asm ("rotlw %0,%0,%2"
-      :"=r" (word)
-      :"0" (word),"r" (i));
-   return word;
-}
-
-static inline uint32_t ROLc(uint32_t word, const int i)
-{
-   asm ("rotlwi %0,%0,%2"
-      :"=r" (word)
-      :"0" (word),"I" (i));
-   return word;
-}
-
-#else
-
 /* rotates the hard way */
 #define ROL(x, y) ( (((uint32_t)(x)<<(uint32_t)((y)&31)) | (((uint32_t)(x)&0xFFFFFFFFUL)>>(uint32_t)(32-((y)&31)))) & 0xFFFFFFFFUL)
 #define ROLc(x, y) ( (((uint32_t)(x)<<(uint32_t)((y)&31)) | (((uint32_t)(x)&0xFFFFFFFFUL)>>(uint32_t)(32-((y)&31)))) & 0xFFFFFFFFUL)
